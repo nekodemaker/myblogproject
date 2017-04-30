65440a4600c581390a3939c6423d00d4de26603e
Sodara NHEK <sodara.nhek@supinternet.fr>
2017-04-30 23:56:00 +0200

    last commit

--- blog_project.sql
+++ blog_project.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Client :  localhost:8889
--- Généré le :  Mer 12 Avril 2017 à 14:23
--- Version du serveur :  5.6.35
--- Version de PHP :  7.0.15
+-- Client :  127.0.0.1
+-- Généré le :  Dim 30 Avril 2017 à 21:55
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de données :  `blog_projet`
+-- Base de données :  `blog_project`
 --
 
 -- --------------------------------------------------------
@@ -22,9 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `id` int(11) NOT NULL,
-  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `text` varchar(1500) COLLATE utf8_unicode_ci NOT NULL
+  `title_article` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `author_article` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date_article` datetime NOT NULL,
+  `text_article` varchar(1500) COLLATE utf8_unicode_ci NOT NULL,
+  `picture_article` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -35,10 +43,10 @@ CREATE TABLE `articles` (
 
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
-  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `id_articles` int(11) NOT NULL,
-  `text` varchar(1500) COLLATE utf8_unicode_ci NOT NULL
+  `author_comment` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date_comment` datetime NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `text_comment` varchar(1500) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -53,6 +61,13 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(7, 'marc', '$2y$10$c2FsdHlzYWx0eXNhbHR5cu8yAa4gGBWUuQX/laAE8Ik6Nllg3nSNu', 'marc@gfdg');
 
 --
 -- Index pour les tables exportées
@@ -86,14 +101,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
